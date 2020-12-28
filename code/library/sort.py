4ecead351a38eb8e07b86056193df407c448d94c
lpmatos <luccapessoamatos@gmail.com>
2020-12-28 13:51:25 -0300

    feat: include bubble sort method to class sort

--- code/library/sort.py
+++ code/library/sort.py
@@ -1,2 +1,14 @@
+from typing import List, NoReturn
+from dataclasses import dataclass, field
+
+@dataclass
 class Sort:
-  pass
+  swap: int = field(default=1, repr=False)
+
+  def bubble_sort(self, array: List) -> NoReturn:
+    while self.swap:
+      self.swap = 0
+      for atual in range(len(array) - 1):
+        if array[atual] > array[atual + 1]:
+          array[atual], array[atual + 1], self.swap = array[atual + 1], array[atual], 1
+    return array
--- code/main.py
+++ code/main.py
@@ -1,5 +1,9 @@
 from functions import base
+from library import sort
 
 if __name__ == "__main__":
   array =  [22, 100, 4, 46, 99, 50, 2, -1, 17, 5, 23, 1, 82, 3]
-  print(base.base_sort(array))
+  sort = sort.Sort()
+
+  print(f"Base sort:      {base.base_sort(array)}")
+  print(f"Bubble sort:    {sort.bubble_sort(array)}")
